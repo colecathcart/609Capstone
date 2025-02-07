a214ba22633c6dd2ea90fef286b795901b78576d
Destin Saba <129232125+destinsaba@users.noreply.github.com>
2025-02-06 18:14:12 -0700

    process_events() and log_event()

--- app/EventDetector.cpp
+++ app/EventDetector.cpp
@@ -44,5 +44,49 @@ class EventDetector {
         }
 
         void process_events() {
-            
+            char buffer[4096];
+            ssize_t length;
+
+            while ((length = read(fanotify_fd, buffer, sizeof(buffer))) > 0) {
+                struct fanotify_event_metadata *metadata = (struct fanotify_event_metadata *)buffer;
+                while (FAN_EVENT_OK(metadata, length)) {
+                    if (metadata->vers != FANOTIFY_METADATA_VERSION) {
+                        std::cerr << "Invalid fanotify metadata version\n";
+                        exit(EXIT_FAILURE);
+                    }
+
+                    if (metadata->fd >= 0) {
+                        char filepath[PATH_MAX];
+                        sprintf(filepath, "/proc/self/fd/%d", metadata->fd);
+                        char resolved_path[PATH_MAX];
+                        ssize_t path_length = readlink(filepath, resolved_path, PATH_MAX);
+                        resolved_path[path_length] = '\0';
+                        close(metadata->fd);
+
+                        std::string event_type;
+                        if (metadata->mask & FAN_CREATE) event_type = "write";    
+                        else if (metadata->mask & FAN_DELETE) event_type = "delete";
+                        else continue;
+
+                        std::string path_str(resolved_path);
+                        size_t last_slash = path_str.find_last_of("/");
+                        std::string filename = path_str.substr(last_slash + 1);
+                        std::string extension = filename.substr(filename.find_last_of(".") + 1);
+
+                        Event event = {event_type, path_str, filename, extension, std::time(nullptr)};
+                        log_event(event);
+                        enqueue_event(event);
+                    }
+                    metadata = FAN_EVENT_NEXT(metadata, length);
+                }
+            }
         }
+
+        void loq_event(const Event& event) {
+            if (event_queue.size() >= max_queue_size) {
+                event_queue.pop();
+            }
+            event_queue.push(event);
+        }
+
+        
