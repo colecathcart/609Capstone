14e7f62b125b251fce681801e170a8a571f7396f
destinsaba <destinsaba9@gmail.com>
2025-03-22 22:13:54 -0600

    remove /n from guimsg

--- app/src/logger.cpp
+++ app/src/logger.cpp
@@ -51,7 +51,7 @@ void Logger::log(const string& message, int whereto) {
             logFile << message << endl;
         }
     } else if(whereto == 2) {
-        string guimsg = message + '\n';
+        string guimsg = message;
         mq_send(mq, guimsg.c_str(), guimsg.size() + 1, 0);
     } else if(whereto == 3) {
         cout << message << endl;
